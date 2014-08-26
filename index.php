6e60e403174bc117ca757226b09e00856bbc79a8
Dubstepper <Dubstepper@users.noreply.github.com>
2014-08-26 15:05:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html> <!-- I don't REALLY know what this does but every html page needs one -->
-<html>
+<html lang="en">
     <head> <!-- Info, title, links to other resources (other files), etc, go here-->
         
         <title>An awesome chat!</title>
         <link rel="stylesheet" type="text/css" href="style.css"> <!-- link to a css file, so we can design stuff :) -->
+        <meta charset="UTF-8"> <!-- specify the character encoding-->
         
     </head>
     <body>
