51e927dc1a08323c684d87d26b4cca773f7bff2d
Dubstepper <Dubstepper@users.noreply.github.com>
2014-08-26 14:52:22 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> <!-- I don't REALLY know what this does but every html page needs one -->
+<html>
+    <head> <!-- Info, title, links to other resources (other files), etc, go here-->
+        
+        <title>An awesome chat!</title>
+        <link rel="stylesheet" type="text/css" href="style.css"> <!-- link to a css file, so we can design stuff :) -->
+        
+    </head>
+    <body>
+        
+        <nav>
+            <ul> <!-- unordered list -->
+                <li>Home</li>  <!-- list item  -->
+                <li>Chat</li>
+                <li>Rules</li>
+                <li>Sign in</li>
+            </ul>
+        </nav>
+        
+    </body>
+</html>
